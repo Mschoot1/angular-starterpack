d43ed3379f89f369764a8ddbbd9d8a4c2dc55d5c
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-11-24 11:58:58 +0100

    server.js added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { UserService } from '../../services/user.service';
-import { User } from '../../models/user.model';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {User} from '../../models/user.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,18 +11,19 @@ export class DashboardComponent implements OnInit {
 
   title = 'Dashboard';
   users: User[];
+  hasUsers = true;
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private userService: UserService
-  ) { }
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private userService: UserService) {
+  }
 
   ngOnInit(): void {
     this.userService.getUsers()
-      .then(users => this.users = users)
+      .then(users => {
+        this.users = users;
+        this.hasUsers = users.length > 0;
+      })
       .catch(error => console.log(error));
   }
-
-
 }
